3e13e2357d4e756d7b22cfdb49df9783f86c65a7
MarsyaAmalia <marsyaamalia505@gmail.com>
2025-10-15 23:30:04 +0800

    Update uts_soal1.java

--- uts_soal1.java
+++ uts_soal1.java
@@ -7,23 +7,27 @@ public class  day12{
      double modalama= isc.nextDouble();
      int lamainvestasi= isc.nextInt();
      double keuntungan = modalama *3/100;
-     int modalbaru= (int)modalama;
-     int tamp= 0;
+     double modalbaru= modalama;
+     double tamp= 0;
      
      for (int i = 1;i<=lamainvestasi;i++ ){
        modalbaru+=keuntungan;
        
-       System.out.println("Saldo hari ke-"+i+": "+modalbaru);
-       tamp+=modalbaru;
+       System.out.printf("%nhari ke- %d : Saldo= Rp%.2f",i,modalbaru);
+       //tamp+=modalbaru;
       
-     }  System.out.println("total keuntungan bersih: "+tamp);
+     } 
+      System.out.printf("%nModal awal : %.2f%n",modalama);
+      System.out.printf("%nsaldo akhir: %.2f%n",modalbaru);
+     System.out.printf("%ntotal keuntungan bersih: %.2f%n",(modalbaru-modalama));
      
-      if(modalbaru>modalama){
+      if(modalbaru>modalama*2){
          System.out.println("Invertasi sangat mengutungkan");
-       }else if(modalbaru>modalama*1.5 && modalbaru<modalama*2 ){
+       }else if(modalbaru<modalama*2 && modalbaru>modalama*1.5 ){
          System.out.println("Investasi menguntungkan");
        }else{
          System.out.println("Kurang optimal");
        }
      
-     
+    }
+}
