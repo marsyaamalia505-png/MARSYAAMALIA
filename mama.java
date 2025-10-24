9a7c54d5978f5abd8f0d84868028c97065e1f832
MarsyaAmalia <marsyaamalia505@gmail.com>
2025-10-24 12:27:36 +0800

    Update and rename .gitignore to mama.java

---
+++ mama.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class  Day47{
+    public static void main(String[] args) {
+     Scanner isc = new Scanner (System.in);
+     
+      String jadwal= isc.nextLine();
+     
+      switch (jadwal){
+        case "senin":
+          System.out.print("Msukkan kegiatan hari senin: ");
+          String p= isc.nextLine();
+        case "selasa":
+          System.out.print("Msukkan kegiatan hari selasa: ");
+          String w= isc.nextLine();
+        case "rabu":
+          System.out.print("Msukkan kegiatan hari rabu:  ");
+          String o= isc.nextLine();
+          case "kamis":
+          System.out.print("Msukkan kegiatan hari kamis: ");
+          String k= isc.nextLine();
+        case "jumat":
+          System.out.print("Msukkan kegiatan hari jumat: ");
+          String c= isc.nextLine();
+        case "sabtu":
+          System.out.print("Msukkan kegiatan hari sabtu:  ");
+          String d= isc.nextLine();
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+      }
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+  
