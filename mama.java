f2ecf80e9d7ab31554d3d9b34ef2debcd204ac69
MarsyaAmalia <marsyaamalia505@gmail.com>
2025-10-24 15:52:39 +0800

    Update mama.java

--- mama.java
+++ mama.java
@@ -4,57 +4,58 @@ public class  Day47{
     public static void main(String[] args) {
      Scanner isc = new Scanner (System.in);
      
+      System.out.println("Masukkan jadwal anda: ");
+      System.out.println("senin");
+      String a= isc.nextLine();
+       System.out.println("selasa");
+      String b= isc.nextLine();
+       System.out.println("rabu");
+      String c= isc.nextLine();
+       System.out.println("kamis");
+      String d= isc.nextLine();
+       System.out.println("jumat");
+      String e= isc.nextLine();
+       System.out.println("sabtu");
+      String f= isc.nextLine();
+       System.out.println("minggu");
+      String g= isc.nextLine();
+      
+      
+      System.out.println("Masukkan hari sekarang apa? ");
+      //isc.nextLine();
+     
       String jadwal= isc.nextLine();
      
       switch (jadwal){
         case "senin":
-          System.out.print("Msukkan kegiatan hari senin: ");
-          String p= isc.nextLine();
+          System.out.print("Jadwal anda adalah "+a);
+          break;
         case "selasa":
-          System.out.print("Msukkan kegiatan hari selasa: ");
-          String w= isc.nextLine();
+         System.out.print("Jadwal anda adalah "+b);
+          break;
         case "rabu":
-          System.out.print("Msukkan kegiatan hari rabu:  ");
-          String o= isc.nextLine();
-          case "kamis":
-          System.out.print("Msukkan kegiatan hari kamis: ");
-          String k= isc.nextLine();
-        case "jumat":
-          System.out.print("Msukkan kegiatan hari jumat: ");
-          String c= isc.nextLine();
+         System.out.print("Jadwal anda adalah "+c);
+         break;
         case "sabtu":
-          System.out.print("Msukkan kegiatan hari sabtu:  ");
-          String d= isc.nextLine();
+          System.out.print("Jadwal anda adalah "+f);
+          break;
+          
+          default:
+          if (jadwal.equalsIgnoreCase("jumat")||jadwal.equalsIgnoreCase("minggu")){
+            System.out.println("jadwal anda adalah "+e);
+            
+          } else if (jadwal.equalsIgnoreCase("kamis")){
+            System.out.println("jadwal anda adalah "+d); 
+            
+          }else {
+            System.out.println("tolong masukkan hari senin hingga minggu saja");
+          }
           
           
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
+  
           
       }
+   
       
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-  
+    }
+}
